440f9396a55baefcecca14c70a467ad4cb2a30a2
Alex <alexandru.iliescu@fortech.ro>
2022-01-25 18:39:59 +0200

    Test commit

--- src/main/java/practice/PracticeArrays.java
+++ src/main/java/practice/PracticeArrays.java
@@ -15,6 +15,7 @@ public class PracticeArrays {
     }
 
     public static void main(String[] args) {
+        //test 
         int[] testArray = {3, 9, 50, 15, 99, 7, 98, 65};
         int result = distClosestNumbers(testArray);
         System.out.println(result); // Expected result is 1 (the 2 closest numbers are 98 and 99)
