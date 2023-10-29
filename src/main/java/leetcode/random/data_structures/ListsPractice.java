0722b4877dc62f312b617dd4f662fc344967f94d
Alex <alexandru.iliescu@fortechproducts.ro>
2023-10-29 23:25:45 +0200

    Update

---
+++ src/main/java/leetcode/random/data_structures/ListsPractice.java
@@ -0,0 +1,18 @@
+package leetcode.random.data_structures;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ListsPractice {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(1);
+        list.add(7);
+        list.add(5);
+
+        Collections.sort(list);
+    }
+}
